838050c248bbb3889277bd11a78e514a8617f05f
Kapustin Alex <a.kapustin@dlg.im>
2021-01-15 11:54:53 +0300

    1

--- TimeProcessing/Tests/test_DateTimeAnalyser.py
+++ TimeProcessing/Tests/test_DateTimeAnalyser.py
@@ -1,31 +1,34 @@
 from TimeProcessing.DateTimeAnalyser import *
 from datetime import time as dtime
 
-text = ["напомнить в полседьмого купить арбуз",
-        "в среду в19.30 позвонить Сергею",
-        "позвонить Сергею завтра в 3 часа дня",
+text = [
+
         "через 3 минуты позвонить Сергею", "через три дня в 19:30 позвонить Сергею",
         "Напомни завтра в девятнадцать тридцать позвонить Сергею", "через 2 дня утром позвонить Сергею",
         "Каждую среду и пятницу в19:30 позвонить Сергею", "Напомни завтра в 19 30 позвонить Сергею",
         "завтра в девятнадцать часов тридцать пять минут сорок секунд вечера позвонить Сергею",
         "завтра часов в девять вечера позвонить Сергею",
         ]
-data = [["напомнить в полседьмого купить арбуз", [dtime(6, 30), "купить арбуз"]]]
+data_absolute_time = [["напомнить в полседьмого купить арбуз", [dtime(6, 30), "купить арбуз"]],
+                      ["в среду в19.30 позвонить Сергею", [dtime(19, 30), "позвонить сергею"]],
+                      ["позвонить Сергею завтра в 3 часа дня", [dtime(15, 00), "позвонить сергею"]]
+                      ]
 
 
 class test_DateTimeAnalyser:
     def __init__(self):
         self.TE = DateTimeAnalyser()
 
-    def process(self):
-        r = self.TE.process(data[0][0])
-        result = [dtime(r[0].hour, r[0].minute), r[2]]
-        flag = result == data[0][1]
-        assert flag
+    def process_absolute_time(self):
+        for dat in data_absolute_time:
+            r = self.TE.process(dat[0])
+            result = [dtime(r[0].hour, r[0].minute), r[2]]
+            flag = result == dat[1]
+            assert flag
 
 
 
 
 def test_process():
     Tester = test_DateTimeAnalyser()
-    Tester.process()
+    Tester.process_absolute_time()
